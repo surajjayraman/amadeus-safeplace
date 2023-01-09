08488671c566fe64e72c26ea4afa1552cdf8b1e6
surajjayraman <suraj.jayraman@gmail.com>
2023-01-09 12:29:02 -0500

    Update amadeus_service.py file.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import amadeus_service
 app = Flask(__name__)
+
 @app.route('/')
 def safeplace():
-   return 'Safe Place'
+    data = amadeus_service.safety_rated_locations()
+    print(data)
+    return render_template('safeplace.html', safeplaces=data)
+
 if __name__ == '__main__':
    app.run()
